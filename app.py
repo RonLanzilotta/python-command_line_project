8a37d03a705c0291b0288d57ad258bdeeb6ab827
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-02-03 17:02:23 -0500

    db seeded

--- app.py
+++ app.py
@@ -8,8 +8,46 @@ class BaseModel(Model):
     class Meta:
         database = db
 
-class Song(BaseModel):
-    title = CharField(max_length=255)
-    artist = CharField(max_length=255)
-    key = CharField(max_length=3)
-    tempo = IntegerField(max_length=3)
+class Songs(BaseModel):
+    title = CharField()
+    artist = CharField()
+    key = CharField()
+    tempo = IntegerField()
+
+db.connect()
+db.drop_tables([Songs])
+db.create_tables([Songs])
+
+Songs(title='Crazy In Love', artist='Beyoncé', key='D-', tempo=100).save()
+Songs(title='Watermelon Sugar', artist='Harry Styles', key='C', tempo=95).save()
+Songs(title='How Deep Is Your Love', artist='Bee Gees', key='Eb', tempo=105).save()
+
+app = Flask(__name__)
+
+@app.route('/songs/', methods=['GET', 'POST'])
+@app.route('/songs/<id>', methods=['GET', 'PUT', 'DELETE'])
+def endpoint(id=None):
+    if request.method == 'GET':
+        if id:
+            return jsonify(model_to_dict(Songs.get(Songs.id == id)))
+        else:
+            songs_list = []
+            for song in Songs.select():
+                songs_list.append(model_to_dict(song))
+            return jsonify(songs_list)
+
+    if request.method == 'PUT':
+        body = request.get_json()
+        Songs.update(body).where(Songs.id == id).execute()
+        return "Song " + str(song.title) + " has been updated."
+
+    if request.method == 'POST':
+        new_song = dict_to_model(Songs, request.get_json())
+        new_song.save()
+        return jsonify({"success": True})
+
+    if request.method == 'DELETE':
+        Songs.delete().where(Songs.id == id).execute()
+        return "Song " + str(song.title) + " deleted."
+
+app.run(debug=True, port=3333)
--- song_schema.sql
+++ song_schema.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS songs;
 
 CREATE TABLE songs (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(255)
-    artist VARCHAR(255)
-    key VARCHAR(3)
+    title VARCHAR(255),
+    artist VARCHAR(255),
+    key VARCHAR(3),
     tempo INTEGER
 )
